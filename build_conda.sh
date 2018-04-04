c448c5bf79807dcbe56c2518a9ca6f840691af43
Ed Beard <ejb207@cam.ac.uk>
2018-04-04 15:11:56 +0100

    Conda build - initial commit

---
+++ build_conda.sh
@@ -0,0 +1,6 @@
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+conda create -q -n cde pip
+pip install -r requirements.txt
+pytest 
